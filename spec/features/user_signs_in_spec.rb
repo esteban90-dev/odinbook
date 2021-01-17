0ee3771a23176e483bd2d4ada3b1c4b95378f99e
Stephen Allard <stephencallard@gmail.com>
2021-01-17 12:44:05 -0600

    Add feature - user signs in

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature "user signs in" do 
+
+  scenario "successfully" do 
+    user = FactoryBot.create(:user)
+
+    visit root_path
+    click_on "sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_on "Sign in"
+
+    expect(page).to have_content("Signed in successfully")
+    expect(page).to have_content("signed in as #{user.email}")
+  end
+
+end
