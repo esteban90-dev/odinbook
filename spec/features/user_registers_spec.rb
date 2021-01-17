a8a8f66e38d0ad682bb5c5b9a2ca12369fab9d1d
Stephen Allard <stephencallard@gmail.com>
2021-01-17 12:31:12 -0600

    Add feature - user registers successfully

---
+++ spec/features/user_registers_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "user registers" do
+
+  scenario "successfully" do
+    visit root_path
+
+    click_on "sign up"
+    fill_in "Email", with: "somebody@example.com"
+    fill_in "Password", with: "testpassword"
+    fill_in "Password confirmation", with: "testpassword"
+    click_on "Sign up"
+
+    expect(page).to have_content("You have signed up successfully")
+    expect(page).to have_content("Logged in as somebody@example.com")
+  end
+
+end
