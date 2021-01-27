88d5b1fa55edd46a07914096a75bf2b78ec0bea8
Stephen Allard <stephencallard@gmail.com>
2021-01-26 20:57:25 -0600

    Add scenario - user unsuccessfully logs in because password is too short

--- spec/features/registration/regular_users/regular_user_registers_spec.rb
+++ spec/features/registration/regular_users/regular_user_registers_spec.rb
@@ -17,4 +17,17 @@ feature "regular user registers" do
     expect(page).not_to have_content("somebody@example.com")
   end
 
+  scenario "unsuccessfully - password must be at least 8 characters" do
+    visit root_path
+
+    click_on "sign up"
+    fill_in "Name", with: "somebody"
+    fill_in "Email", with: "somebody@example.com"
+    fill_in "Password", with: "1234567"
+    fill_in "Password confirmation", with: "1234567"
+    click_on "Sign up"
+
+    expect(page).to have_content("Password is too short")
+  end 
+  
 end
