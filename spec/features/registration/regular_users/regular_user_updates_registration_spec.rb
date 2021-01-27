e05fa68b6e384179319bf78212e74aed3f9f4360
Stephen Allard <stephencallard@gmail.com>
2021-01-26 21:31:51 -0600

    Change user to regular_user

--- spec/features/registration/regular_users/regular_user_updates_registration_spec.rb
+++ spec/features/registration/regular_users/regular_user_updates_registration_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 feature "regular user updates registration" do
 
   scenario "changes name and email successfully" do
-    user = FactoryBot.create(:user)
+    regular_user = FactoryBot.create(:user)
     
-    sign_in user
+    sign_in regular_user
     visit edit_user_registration_path
 
     fill_in "Name", with: "somebody else"
     fill_in "Email", with: "somebody_else@example.com"
-    fill_in "Current password", with: "#{user.password}"
+    fill_in "Current password", with: "#{regular_user.password}"
     click_on "Update"
 
     expect(page).to have_content("Your account has been updated successfully")
@@ -18,14 +18,14 @@ feature "regular user updates registration" do
   end
 
   scenario "changes password successfully" do
-    user = FactoryBot.create(:user)
+    regular_user = FactoryBot.create(:user)
     
-    sign_in user
+    sign_in regular_user
     visit edit_user_registration_path
 
     fill_in "Password", with: "testpassword2"
     fill_in "Password confirmation", with: "testpassword2"
-    fill_in "Current password", with: "#{user.password}"
+    fill_in "Current password", with: "#{regular_user.password}"
     click_on "Update"
 
     expect(page).to have_content("Your account has been updated successfully")
