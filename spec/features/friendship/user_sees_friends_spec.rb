b29f4102fea75ec6815087b4b8aec45d3fecb508
Stephen Allard <stephencallard@gmail.com>
2021-01-31 09:09:51 -0600

    Remove space

--- spec/features/friendship/user_sees_friends_spec.rb
+++ spec/features/friendship/user_sees_friends_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature "user sees friends" do
 
   scenario "successfully" do 
-
     joe = FactoryBot.create(:user, name: "joe", email: "joe@mail.com")
     bob = FactoryBot.create(:user, name: "bob", email: "bob@mail.com")
     joe.friends << bob
