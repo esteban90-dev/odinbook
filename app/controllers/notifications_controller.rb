05a63a45fc5ba476b523f3dbd104b1fe0939e4f3
Stephen Allard <stephencallard@gmail.com>
2021-02-04 21:34:42 -0600

    Modify feature - user sees notifications in descending order

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications.each{ |notification| notification.touch if notification.unacked? }
+    @notifications = current_user.notifications.order(created_at: :desc)
+    @notifications.each{ |notification| notification.touch if notification.unacked? }
   end
 end
--- spec/support/notification_helper.rb
+++ spec/support/notification_helper.rb
@@ -3,3 +3,7 @@ require 'rails_helper'
 def have_new_notification
   have_css(".nav", text: "new notification")
 end
+
+def notifications_descending?(notification_1, notification_2)
+  page.body.index(notification_2.message) < page.body.index(notification_1.message)
+end
