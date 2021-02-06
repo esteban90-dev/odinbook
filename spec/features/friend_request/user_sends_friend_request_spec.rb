b4f045ef0028b79b7b2485fd73c540f44b8fd607
Stephen Allard <stephencallard@gmail.com>
2021-02-06 09:20:37 -0600

    Refactor 'user sends friend request' spec

--- spec/features/friend_request/user_sends_friend_request_spec.rb
+++ spec/features/friend_request/user_sends_friend_request_spec.rb
@@ -2,16 +2,27 @@ require 'rails_helper'
 
 feature "user sends friend request" do
 
-  scenario "successfully" do
-    bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
-    joe = FactoryBot.create(:user, name: "joe", email: "joe@example.com")
+  before do 
+    @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+    @joe = FactoryBot.create(:user, name: "joe", email: "joe@example.com")
 
-    sign_in bob
+    sign_in @bob
     visit users_path 
-    add_friend(joe)
+    add_friend(@joe)
+  end
 
+  scenario "and sees a flash message" do
     expect(page).to have_content("Friend request successfully sent to joe")
-    expect(request_pending?(joe)).to eq(true)
+  end
+
+  scenario "and sees 'friend request sent' next to the user in the users index" do 
+    expect(friend_request_pending?(@joe)).to be(true)
+  end
+
+  scenario "and sees it appear in the sent section of the friend requests index" do 
+    visit friend_requests_path
+
+    expect(page).to have_sent_friend_request(@joe)
   end
 
 end
--- spec/support/friend_request_helper.rb
+++ spec/support/friend_request_helper.rb
@@ -12,15 +12,15 @@ def ignore_friend_request(user)
   find("##{user.id}", text: user.name).click_on "Ignore"
 end
 
-def request_pending?(user)
+def friend_request_pending?(user)
   user_section = find("##{user.id}")
-  expect(user_section).to have_no_link("Add friend").and have_content("Request Pending")
+  expect(user_section).to have_no_link("Add friend").and have_content("friend request sent")
 end
 
 def have_incoming_friend_request(request)
   have_css(".incoming", text: request.requestor.name)
 end
 
-def have_sent_friend_request(request)
-  have_css(".sent", text: request.requestee.name)
+def have_sent_friend_request(user)
+  have_css(".sent", text: user.name)
 end
