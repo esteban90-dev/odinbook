79fdbdabe6a82e363efecab8a768e1bcee73a536
Stephen Allard <stephencallard@gmail.com>
2021-02-09 21:04:38 -0600

    Refactor

--- spec/features/registration/facebook_users/facebook_user_deletes_account_spec.rb
+++ spec/features/registration/facebook_users/facebook_user_deletes_account_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 feature "facebook user deletes account" do
 
-  scenario "successfully" do 
-
-    facebook_user = FactoryBot.create(:user, :from_facebook)
-    sign_in facebook_user
+  before(:each) do 
+    @facebook_user = FactoryBot.create(:user, :from_facebook)
+    sign_in @facebook_user
 
     visit edit_user_registration_path 
     click_on "Cancel my account"
+  end
 
+  scenario "they see a flash message" do 
     expect(page).to have_content("Your account has been successfully cancelled")
-    expect(User.all).not_to include(facebook_user)
+  end
+    
+  scenario "the user no longer exists" do 
+    expect(User.all).not_to include(@facebook_user)
   end
 
 end
--- spec/features/registration/facebook_users/facebook_user_updates_registration_spec.rb
+++ spec/features/registration/facebook_users/facebook_user_updates_registration_spec.rb
@@ -2,17 +2,29 @@ require 'rails_helper'
 
 feature "facebook user updates registration" do
 
-  scenario "changes name and email successfully" do
-    facebook_user = FactoryBot.create(:user, :from_facebook)
-    sign_in facebook_user
+  before(:each) do
+    @facebook_user = FactoryBot.create(:user, :from_facebook)
+    sign_in @facebook_user
 
-    visit edit_user_registration_path
+    visit root_path
+    click_on "edit account"
     fill_in "Name", with: "somebody else"
     fill_in "Email", with: "somebody_else@example.com"
     click_on "Update"
+  end
 
+  scenario "they see a flash message" do 
     expect(page).to have_content("Your account has been updated successfully")
+  end
+
+  scenario "they see that they are logged in with a different name" do 
     expect(page).to have_content("signed in as somebody else")
   end
 
+  scenario "they see their account credentials change" do 
+    click_on "edit account"
+    
+    expect(page).to have_field('user_name', with: "somebody else")
+    expect(page).to have_field('user_email', with: "somebody_else@example.com")
+  end
 end
