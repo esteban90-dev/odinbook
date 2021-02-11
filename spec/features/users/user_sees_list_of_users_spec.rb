a23f3dcc68dccc2e2aae555d56edca8f72a8d07b
Stephen Allard <stephencallard@gmail.com>
2021-02-10 20:49:54 -0600

    refactor

--- spec/features/users/user_sees_list_of_users_spec.rb
+++ spec/features/users/user_sees_list_of_users_spec.rb
@@ -2,18 +2,22 @@ require "rails_helper"
 
 feature "user sees list of other users" do 
 
-  scenario "successfully" do 
+  before(:each) do 
+    @bob = FactoryBot.create(:user, name: "bob", email: "bob@mail.com")
+    @frank = FactoryBot.create(:user, name: "frank", email: "frank@mail.com")
+    @john = FactoryBot.create(:user, name: "john", email: "john@mail.com")
 
-    bob = FactoryBot.create(:user, name: "bob", email: "bob@mail.com")
-    frank = FactoryBot.create(:user, name: "frank", email: "frank@mail.com")
-    john = FactoryBot.create(:user, name: "john", email: "john@mail.com")
-
-    sign_in bob
+    sign_in @bob
     visit users_path
+  end
 
-    expect(page).not_to have_user(bob)
-    expect(page).to have_user(frank)
-    expect(page).to have_user(john)
+  scenario "user does not see themselves in the list" do 
+    expect(page).not_to have_user(@bob)
+  end
+
+  scenario "user sees other users in the list" do   
+    expect(page).to have_user(@frank)
+    expect(page).to have_user(@john)
   end
 
 end
