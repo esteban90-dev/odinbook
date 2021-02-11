4bbfae717fcf1bf565ee9713450400b701ad3697
Stephen Allard <stephencallard@gmail.com>
2021-02-10 19:53:16 -0600

    Refactor

--- spec/features/registration/regular_users/regular_user_deletes_account_spec.rb
+++ spec/features/registration/regular_users/regular_user_deletes_account_spec.rb
@@ -2,17 +2,21 @@ require 'rails_helper'
 
 feature "regular user deletes account" do
 
-  scenario "successfully" do
-
-    regular_user = FactoryBot.create(:user)
-    sign_in regular_user
+  before(:each) do
+    @regular_user = FactoryBot.create(:user)
+    sign_in @regular_user
 
     visit edit_user_registration_path
     
     click_on "Cancel my account"
+  end
 
+  scenario "they see a flash message" do 
     expect(page).to have_content("Your account has been successfully cancelled")
-    expect(User.all).not_to include(regular_user)
+  end
+
+  scenario "the user no longer exists" do 
+    expect(User.all).not_to include(@regular_user)
   end
 
 end
