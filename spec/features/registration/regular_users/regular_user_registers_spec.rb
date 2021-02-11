ec5469c4541a6a5b183b0f28f77f97f5673cfa7b
Stephen Allard <stephencallard@gmail.com>
2021-02-10 20:07:58 -0600

    refactor

--- spec/features/registration/regular_users/regular_user_registers_spec.rb
+++ spec/features/registration/regular_users/regular_user_registers_spec.rb
@@ -2,47 +2,66 @@ require 'rails_helper'
 
 feature "regular user registers" do
 
-  scenario "successfully" do
-    visit root_path
+  context "successfully" do 
 
-    click_on "sign up"
-    fill_in "Name", with: "somebody"
-    fill_in "Email", with: "somebody@example.com"
-    fill_in "Password", with: "testpassword"
-    fill_in "Password confirmation", with: "testpassword"
-    click_on "Sign up"
+    before(:each) do
+      visit root_path
 
-    expect(page).to have_content("You have signed up successfully")
-    expect(page).to have_content("signed in as somebody")
-    expect(page).not_to have_content("somebody@example.com")
+      click_on "sign up"
+      fill_in "Name", with: "somebody"
+      fill_in "Email", with: "somebody@example.com"
+      fill_in "Password", with: "testpassword"
+      fill_in "Password confirmation", with: "testpassword"
+      click_on "Sign up"
+    end
+
+    scenario "they see a flash message" do 
+      expect(page).to have_content("You have signed up successfully")
+    end
+      
+    scenario "they see that they are logged in" do 
+      expect(page).to have_content("signed in as somebody")
+    end
+
+    scenario "they see their account credentials" do 
+      click_on "edit account"
+
+      expect(page).to have_field('user_name', with: "somebody")
+      expect(page).to have_field('user_email', with: "somebody@example.com")
+    end
+
   end
 
-  scenario "unsuccessfully - password must be at least 8 characters" do
-    visit root_path
+  context "unsuccessfully" do 
 
-    click_on "sign up"
-    fill_in "Name", with: "somebody"
-    fill_in "Email", with: "somebody@example.com"
-    fill_in "Password", with: "1234567"
-    fill_in "Password confirmation", with: "1234567"
-    click_on "Sign up"
+    scenario "they see that their password must be at least 8 characters" do
+      visit root_path
 
-    expect(page).to have_content("Password is too short")
-  end
-  
-  scenario "unsuccessfully - email has already been taken" do
-    FactoryBot.create(:user)
+      click_on "sign up"
+      fill_in "Name", with: "somebody"
+      fill_in "Email", with: "somebody@example.com"
+      fill_in "Password", with: "1234567"
+      fill_in "Password confirmation", with: "1234567"
+      click_on "Sign up"
+    
+      expect(page).to have_content("Password is too short")
+    end
 
-    visit root_path
+    scenario "they see that the email they entered has already been taken" do
+      FactoryBot.create(:user)
 
-    click_on "sign up"
-    fill_in "Name", with: "somebody"
-    fill_in "Email", with: "somebody@example.com"
-    fill_in "Password", with: "12345678"
-    fill_in "Password confirmation", with: "12345678"
-    click_on "Sign up"
+      visit root_path
 
-    expect(page).to have_content("Email has already been taken")
+      click_on "sign up"
+      fill_in "Name", with: "somebody"
+      fill_in "Email", with: "somebody@example.com"
+      fill_in "Password", with: "12345678"
+      fill_in "Password confirmation", with: "12345678"
+      click_on "Sign up"
+
+      expect(page).to have_content("Email has already been taken")
+    end
+
   end
   
 end
