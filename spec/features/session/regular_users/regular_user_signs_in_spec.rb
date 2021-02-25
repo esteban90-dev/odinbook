78d20d568b2988d431f9550ab3ab26b723ad1c9b
Stephen Allard <stephencallard@gmail.com>
2021-02-25 17:25:20 -0600

    Change context description

--- spec/features/session/regular_users/regular_user_signs_in_spec.rb
+++ spec/features/session/regular_users/regular_user_signs_in_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 feature "regular user signs in" do 
 
-  context "successfully" do 
+  context "with a valid email/password" do 
 
     before(:each) do 
       @user = FactoryBot.create(:user, :with_blank_profile)
@@ -24,7 +24,7 @@ feature "regular user signs in" do
 
   end
 
-  context "unsuccessfully" do
+  context "with an invalid email/password" do
 
     scenario "they see an error message" do 
       user = FactoryBot.create(:user, :with_blank_profile)
