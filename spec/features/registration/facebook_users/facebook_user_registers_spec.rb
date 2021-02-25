1b2f5d5011a4f699f8e8c31a5f0c2a54e0b9b65d
Stephen Allard <stephencallard@gmail.com>
2021-02-25 17:27:37 -0600

    Add 'facebook_user_registers_spec'

---
+++ spec/features/registration/facebook_users/facebook_user_registers_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+feature "facebook user registers" do 
+
+  context "successfully" do 
+
+    before(:each) do
+      stub_omniauth_successful
+
+      visit root_path
+
+      click_on "sign up"
+      click_on "Sign in with Facebook"
+    end
+
+    scenario "they see a flash message" do 
+      expect(page).to have_content("Signed in successfully")
+    end
+      
+    scenario "they see that they are signed in" do 
+      expect(page).to have_content("signed in as somebody")
+    end
+
+    scenario "they see their account credentials" do 
+      click_on "edit account"
+
+      expect(page).to have_field('user_name', with: "somebody")
+      expect(page).to have_field('user_email', with: "somebody@example.com")
+    end
+
+    scenario "they receive a notification with a link that tells them to edit their profile" do 
+      visit root_path
+      click_on "notifications"
+      
+      user_sees_edit_profile_notification
+    end
+
+    scenario "a blank profile is created for them" do 
+      expect(User.first.profile).not_to eq(nil)
+    end
+
+  end
+
+end
