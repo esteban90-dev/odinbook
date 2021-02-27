369973fbde9a14d7a9cb5e61d12f3a9224b25bcf
Stephen Allard <stephencallard@gmail.com>
2021-02-27 10:45:13 -0600

    Move post link to #post_link

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,7 +4,7 @@ class LikesController < ApplicationController
     post.likes.create(liker: current_user)
 
     if post.user != current_user
-      post.user.notifications.create(message: "#{current_user.name} liked your #{view_context.link_to("post", user_profile_path(post.user.id) + "##{post.id}", data: { turbolinks: false } ) }")
+      post.user.notifications.create(message: "#{current_user.name} liked your #{post_link(post)} ")
     end
     
     redirect_to user_profile_path(post.user.id)
@@ -15,5 +15,11 @@ class LikesController < ApplicationController
     like.destroy
 
     redirect_to user_profile_path(like.post.user)
+  end
+
+  private
+
+  def post_link(post)
+    "#{view_context.link_to("post", user_profile_path(post.user.id) + "##{post.id}", data: { turbolinks: false })}"
   end
 end
