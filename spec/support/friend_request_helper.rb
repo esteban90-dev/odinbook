fc3476e08b821480ba43fcde1a8ba6003f0524eb
Stephen Allard <stephencallard@gmail.com>
2021-02-28 10:14:57 -0600

    Extract #have_accept_ignore_requestor_links

--- spec/support/friend_request_helper.rb
+++ spec/support/friend_request_helper.rb
@@ -29,9 +29,7 @@ end
 def user_sees_requestor_in_friend_request_index(requestor)
   requestor_section = find(".incoming ##{requestor.id}")
   expect(requestor_section).to have_content("#{requestor.name}")
-  #add href to verify that accept/ignore links are the same as the links in the users index
-  expect(requestor_section).to have_link("accept", href: "/friend_requests/#{requestor.sent_friend_requests.first.id}/accept")
-  expect(requestor_section).to have_link("ignore", href: "/friend_requests/#{requestor.sent_friend_requests.first.id}/ignore")
+  expect(requestor_section).to have_accept_ignore_requestor_links(requestor)
 end
 
 def user_sees_requestee_addable_in_user_index(requestee)
@@ -50,9 +48,7 @@ end
 def user_sees_requestor_acceptable_ignorable_in_user_index(requestor)
   requestor_section = find("##{requestor.id}")
   expect(requestor_section).to have_content("#{requestor.name}")
-  #add href to verify that accept/ignore links are the same as the links in the friend_requests index
-  expect(requestor_section).to have_link("accept", href: "/friend_requests/#{requestor.sent_friend_requests.first.id}/accept")
-  expect(requestor_section).to have_link("ignore", href: "/friend_requests/#{requestor.sent_friend_requests.first.id}/ignore")
+  expect(requestor_section).to have_accept_ignore_requestor_links(requestor)
 end
 
 def user_sees_requestor_as_friend_in_user_index(requestor)
@@ -66,12 +62,16 @@ def user_sees_incoming_friend_request_notification(requestee)
   request = requestee.incoming_friend_requests.first
   notification_section = find("##{requestee.notifications.last.id}")
   expect(notification_section).to have_content("You have a new friend request from #{request.requestor.name}")
-  expect(notification_section).to have_link("accept", href: "/friend_requests/#{requestee.incoming_friend_requests.first.id}/accept")
-  expect(notification_section).to have_link("ignore", href: "/friend_requests/#{requestee.incoming_friend_requests.first.id}/ignore")
+  expect(notification_section).to have_accept_ignore_requestor_links(request.requestor)
 end
 
 def user_sees_accepted_friend_request_notification(requestee, requestor)
   notification_section = find("##{requestor.notifications.last.id}")
   expect(notification_section).to have_content("#{requestee.name} accepted your friend request")
   expect(notification_section).to have_link("#{requestee.name}", href: "/users/#{requestee.id}/profile")
+end
+
+def have_accept_ignore_requestor_links(requestor)
+  have_link("accept", href: "/friend_requests/#{requestor.sent_friend_requests.first.id}/accept")
+  have_link("ignore", href: "/friend_requests/#{requestor.sent_friend_requests.first.id}/ignore")
 end
