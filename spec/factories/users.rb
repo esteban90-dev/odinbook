2fe8dcf6c3df947a70acd1bc63462e2b473d9ef1
Stephen Allard <stephencallard@gmail.com>
2021-03-16 08:17:54 -0500

    refactor

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,28 +9,5 @@ FactoryBot.define do
       uid { "1234" }
     end
 
-    trait :with_example_profile do 
-      after(:create) do |user| 
-        user.profile.update({
-          location: "New York, NY",
-          education: "Bachelor's Degree",
-          relationship_status: "Single",
-        })
-
-        #add profile picture
-        profile_image_path = Rails.root.join('spec','files','eiffel_tower.jpg')
-        profile_image_file = fixture_file_upload(profile_image_path, 'image/jpg')
-        user.profile.picture.attach(profile_image_file)
-
-        #create a post - text only
-        user.posts.create(body: "this is the first post")
-
-        #create a post - text & picture
-        post_image_path = Rails.root.join('spec','files','empire_state_building.jpg')
-        post_image_file = fixture_file_upload(post_image_path, 'image/jpg')
-        user.posts.create(body: "this is the second post", picture: post_image_file)
-      end
-    end
-
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 describe Post, "#likers" do
 
   it "returns a list of users that have liked the post" do 
-    bob = FactoryBot.create(:user, :with_example_profile, name: "bob", email: "bob@example.com") 
+    bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com") 
     frank = FactoryBot.create(:user, name: "frank", email: "frank@example.com")
     alex = FactoryBot.create(:user, name: "alex", email: "alex@example.com")
 
+    post = bob.posts.create(body: "this is a post")
     bob.friends << [frank, alex]
-    bob.posts.first.likes.create(liker: frank)
-    bob.posts.first.likes.create(liker: alex)
+    post.likes.create(liker: frank)
+    post.likes.create(liker: alex)
 
-    expect(bob.posts.first.likers).to include(frank, alex)
+    expect(post.likers).to include(frank, alex)
   end
 
 end
