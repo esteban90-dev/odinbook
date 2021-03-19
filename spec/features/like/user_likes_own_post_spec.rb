67344d4a4c0ae805367a6320ad5164943573f718
Stephen Allard <stephencallard@gmail.com>
2021-03-19 15:51:16 -0500

    Add test - user likes their own post from the timeline

--- spec/features/like/user_likes_own_post_spec.rb
+++ spec/features/like/user_likes_own_post_spec.rb
@@ -2,17 +2,45 @@ require 'rails_helper'
 
 feature "user likes his/her own post" do 
 
-  scenario "no new notification is generated" do 
-    @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
-    @post = @bob.posts.create(body: "this is a post")
-    sign_in @bob
+  context "from their profile" do 
 
-    visit root_path
-    click_on "bob"
-    like(@post)
-    click_on "notifications"
-  
-    expect(notifications_on_page.count).to eq(0)
+    scenario "no new notification is generated" do 
+      @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+      @post = @bob.posts.create(body: "this is a post")
+      sign_in @bob
+
+      visit root_path
+      click_on "bob"
+      like(@post)
+      click_on "notifications"
+    
+      expect(notifications_on_page.count).to eq(0)
+    end
+
+  end
+
+  context "from the timeline" do 
+
+    before(:each) do 
+      @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+      @post = @bob.posts.create(body: "this is a post")
+      sign_in @bob
+
+      visit root_path
+      click_on "timeline"
+      like(@post)
+    end
+
+    scenario "they are redirected back to the timeline" do 
+      expect(page).to have_current_path(posts_path)
+    end
+
+    scenario "no new notification is generated" do 
+      click_on "notifications" 
+      
+      expect(notifications_on_page.count).to eq(0)
+    end
+
   end
 
 end
