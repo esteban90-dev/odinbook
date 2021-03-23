0408ebaee3e99ba694172a83c94471e6bb326ad2
Stephen Allard <stephencallard@gmail.com>
2021-03-23 14:29:32 -0500

    Add authorization and authentication to Friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,8 @@
 class FriendshipsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_index, only: :index
+  before_action :authorize_destroy, only: :destroy
+
   def index
     user = User.find(params[:user_id])
     if current_user.friends.include?(user) || current_user == user
@@ -12,5 +16,26 @@ class FriendshipsController < ApplicationController
     
     flash[:notice] = "Successfully unfriended #{@friendship.friend.name}"
     redirect_to user_friendships_path(current_user.id)
+  end
+
+  private
+
+  def authorize_index
+    #only allow current user to see his/her friends or his/her friends' friends
+    if User.find(params[:user_id]) != current_user && !current_user.friends.include?(User.find(params[:user_id]))
+      unauthorized
+    end
+  end
+
+  def authorize_destroy
+    #only the user or friend can destroy the record
+    if Friendship.find(params[:id]).user != current_user && Friendship.find(params[:id]).friend != current_user 
+      unauthorized
+    end
+  end
+
+  def unauthorized
+    flash[:alert] = "this action is not permitted"
+    redirect_to posts_path
   end
 end
---
+++ spec/controllers/friendships_controller_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe FriendshipsController, type: :controller do 
+
+  describe "#index" do 
+
+    context "as an unauthenticated user" do 
+      it "redirects to the sign in page" do 
+        get :index, params: { user_id: 1 }
+        
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context "as an unauthorized user" do 
+      before(:each) do 
+        bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+        frank = FactoryBot.create(:user, name: "frank", email: "frank@example.com")
+        john = FactoryBot.create(:user, name: "john", email: "john@example.com")
+        bob.friends << frank
+
+        sign_in john
+        get :index, params: { user_id: bob.id }
+      end
+
+      it "redirects to the posts index" do 
+        expect(response).to redirect_to posts_path
+      end
+
+      it "sets a flash alert message" do 
+        expect(flash[:alert]).to eq("this action is not permitted")
+      end
+    end
+
+  end
+
+  describe "#destroy" do 
+
+    context "as an unauthenticated user" do 
+      it "redirects to the sign in page" do 
+        delete :destroy, params: { id: 1 }
+        
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context "as an unauthorized user" do 
+      before(:each) do 
+        bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+        frank = FactoryBot.create(:user, name: "frank", email: "frank@example.com")
+        john = FactoryBot.create(:user, name: "john", email: "john@example.com")
+        friendship = Friendship.create(user: bob, friend: frank)
+
+        sign_in john
+        delete :destroy, params: { id: friendship.id }
+      end
+
+      it "doesn't destroy the record" do 
+        #there are two records for each friendship (one is a reverse friendship)
+        expect(Friendship.all.count).to eq(2) 
+      end
+
+      it "redirects to the posts index" do 
+        expect(response).to redirect_to posts_path
+      end
+
+      it "sets a flash alert message" do 
+        expect(flash[:alert]).to eq("this action is not permitted")
+      end
+    end
+
+  end
+
+end
