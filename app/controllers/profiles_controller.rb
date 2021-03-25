fd21414173dab630783ad1c50907a8aa2720d138
Stephen Allard <stephencallard@gmail.com>
2021-03-24 20:23:07 -0500

    Refactor - set profile in filter, remove setting user in the edit action because it is unnecessary

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,15 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_profile, only: [:edit, :update]
 
   def show
     @profile = Profile.find_by_user_id(params[:user_id])
   end
 
   def edit
-    @user = current_user
-    @profile = current_user.profile
   end
 
   def update
-    @profile = current_user.profile
-
     if @profile.update(profile_params)
       flash[:notice] = "Successfully updated profile"
       redirect_to user_profile_path(params[:user_id])
@@ -23,5 +20,9 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:profile).permit(:location, :education, :relationship_status, :picture)
+  end
+
+  def set_profile
+    @profile = current_user.profile
   end
 end
