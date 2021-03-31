d9733818daedafa05fd0c7b8f999a5e8cf965375
Stephen Allard <stephencallard@gmail.com>
2021-03-31 09:14:07 -0500

    Add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+EDUCATION_OPTIONS = ["High School", "Associate's Degree", "Bachelor's Degree", "Master's Degree", "Doctoral Degree"]
+RELATIONSHIP_STATUS_OPTIONS = ["Single", "In a Relationship", "Married"]
+
+def random_education
+  EDUCATION_OPTIONS[(rand*EDUCATION_OPTIONS.length).to_i]
+end
+
+def random_relationship_status
+  RELATIONSHIP_STATUS_OPTIONS[(rand*RELATIONSHIP_STATUS_OPTIONS.length).to_i]
+end
+
+def random_user
+  User.order('RANDOM()').first
+end
+
+#create 50 users
+50.times do 
+  #create user
+  user = User.create(
+    email: Faker::Internet.unique.email,
+    name: Faker::Name.name,
+    password: ENV['test_password']
+  )
+
+  puts "user #{user.id} created"
+end
+
+#create a profile for each user
+User.all.each do |user|
+  profile = user.create_profile(
+    location: Faker::Address.city,
+    education: random_education,
+    relationship_status: random_relationship_status
+  )
+
+  #attach profile picture
+  profile.picture.attach(io: File.open('app/assets/images/generic-user-icon-10.png'), filename: 'generic-user-icon-10.png', content_type: 'image/png')
+
+  puts "user #{user.id} profile created"
+end
+
+#create 10 posts per user
+User.all.each do |user|
+  10.times{ user.posts.create(body: Faker::Quote.famous_last_words) }
+
+  puts "user #{user.id} posts created"
+end
+
+#assign each user 5 random friends
+User.all.each do |user|
+  while user.friends.count <= 5
+    random_user = User.all.sample
+    unless user.friends.include?(random_user)
+      user.friends << random_user
+      puts "user #{user.id} is now friends with #{random_user.id}"
+    end
+  end
+end
