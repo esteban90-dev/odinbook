c0555fefb9e054576f2905d18c067d3b3d450413
Stephen Allard <stephencallard@gmail.com>
2021-04-05 09:20:45 -0500

    Modify welcome email message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,6 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
-
   def welcome_email
     @user = params[:user]
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Welcome to Odinbook')
   end
 end
