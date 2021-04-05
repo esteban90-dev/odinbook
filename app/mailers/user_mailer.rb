c47e1c5f6917c86fa96e18bf9299d24c985357ac
Stephen Allard <stephencallard@gmail.com>
2021-04-05 15:18:26 -0500

    pass user as argument to #welcome_email instead of using params

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
-  def welcome_email
-    @user = params[:user]
+  def welcome_email(user)
+    @user = user
     mail(to: @user.email, subject: 'Welcome to Odinbook')
   end
 end
