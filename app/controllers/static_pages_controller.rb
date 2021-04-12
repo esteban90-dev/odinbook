57591a100b24b2a9694bafe0bf56eadc3160b0e5
Stephen Allard <stephencallard@gmail.com>
2021-04-12 10:24:41 -0500

    remove static_pages controller, set posts#index to root route

--- app/controllers/static_pages_controller.rb
+++
@@ -1,6 +0,0 @@
-class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :welcome
-  
-  def welcome
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "static_pages#welcome"
+  root to: "posts#index"
   
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks", registrations: 'registrations' }
 
