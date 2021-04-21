7f9bb95aa7bdcf6e72d896c1a60550b064225bd8
Stephen Allard <stephencallard@gmail.com>
2021-04-20 20:53:19 -0500

    display 'no posts yet' when no posts exist

--- spec/features/post/user_sees_post_index_spec.rb
+++ spec/features/post/user_sees_post_index_spec.rb
@@ -2,56 +2,75 @@ require 'rails_helper'
 
 feature "user sees post index" do 
 
-  before(:each) do 
-    @stephen = FactoryBot.create(:user, name: "stephen", email: "stephen@example.com")
-    @sean = FactoryBot.create(:user, name: "sean", email: "sean@example.com")
-    @kevin = FactoryBot.create(:user, name: "kevin", email: "kevin@example.com") 
+  context "with posts" do 
 
-    @stephen.friends << @sean
+    before(:each) do 
+      @stephen = FactoryBot.create(:user, name: "stephen", email: "stephen@example.com")
+      @sean = FactoryBot.create(:user, name: "sean", email: "sean@example.com")
+      @kevin = FactoryBot.create(:user, name: "kevin", email: "kevin@example.com") 
 
-    @post_1 = @stephen.posts.create(body: "this is the first post")
-    @post_2 = @sean.posts.create(body: "this is the second post")
-    @post_3 = @kevin.posts.create(body: "this is the third post")
+      @stephen.friends << @sean
 
-    sign_in @stephen
-    visit posts_path
-  end
+      @post_1 = @stephen.posts.create(body: "this is the first post")
+      @post_2 = @sean.posts.create(body: "this is the second post")
+      @post_3 = @kevin.posts.create(body: "this is the third post")
 
-  scenario "they see posts belonging to themselves and friends" do
-    expect(page).to have_content(@post_1.body)
-    expect(page).to have_content(@post_2.body)
-  end
-
-  scenario "they don't see an edit link next to a friend's posts" do 
-    post_doesnt_appear_editable(@post_2)
-  end
-
-  scenario "they don't see a delete link next to a friend's posts" do 
-    post_doesnt_appear_deletable(@post_2)
-  end
-
-  scenario "they see each post's author's name" do 
-    expect(page).to have_post_author_name(@post_1)
-    expect(page).to have_post_author_name(@post_2)
-  end
-
-  scenario "they don't see posts belonging to users that aren't friends" do 
-    expect(page).not_to have_content(@post_3.body)
-  end
-
-  scenario "they only see up to 15 posts" do 
-    20.times do 
-      @stephen.posts.create(body: "another post")
+      sign_in @stephen
+      visit posts_path
     end
 
-    visit posts_path
+    scenario "they see posts belonging to themselves and friends" do
+      expect(page).to have_content(@post_1.body)
+      expect(page).to have_content(@post_2.body)
+    end
 
-    expect(posts_on_page.count).to eq(15)
-  end
+    scenario "they don't see an edit link next to a friend's posts" do 
+      post_doesnt_appear_editable(@post_2)
+    end
 
-  scenario "the posts are in order of most recent first" do 
-    expect(page.body.index(@post_2.body) < page.body.index(@post_1.body)).to eq(true)
-    user_sees_content_order_descending(@post_1.body, @post_2.body)
+    scenario "they don't see a delete link next to a friend's posts" do 
+      post_doesnt_appear_deletable(@post_2)
+    end
+
+    scenario "they see each post's author's name" do 
+      expect(page).to have_post_author_name(@post_1)
+      expect(page).to have_post_author_name(@post_2)
+    end
+
+    scenario "they don't see posts belonging to users that aren't friends" do 
+      expect(page).not_to have_content(@post_3.body)
+    end
+
+    scenario "they only see up to 15 posts" do 
+      20.times do 
+        @stephen.posts.create(body: "another post")
+      end
+
+      visit posts_path
+
+      expect(posts_on_page.count).to eq(15)
+    end
+
+    scenario "the posts are in order of most recent first" do 
+      expect(page.body.index(@post_2.body) < page.body.index(@post_1.body)).to eq(true)
+      user_sees_content_order_descending(@post_1.body, @post_2.body)
+    end
+
+  end 
+
+  context "without posts" do 
+
+    scenario "they see 'no posts yet' displayed" do 
+
+      bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+
+      sign_in bob
+      visit root_path
+      click_on "timeline"
+
+      expect(page).to have_content("no posts yet")
+    end
+
   end
 
 end
--- spec/features/profile/user_visits_friends_profile_spec.rb
+++ spec/features/profile/user_visits_friends_profile_spec.rb
@@ -2,60 +2,82 @@ require 'rails_helper'
 
 feature "user visits friend's profile" do 
 
-  before(:each) do 
-    @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
-    @post_1 = @bob.posts.create(body: "this is a post")
-    @post_2 = @bob.posts.create(body: "this is another post")
-    @frank = FactoryBot.create(:user, name: "frank", email: "frank@example.com")
-    @bob.friends << @frank
-    sign_in @frank
+  context "with posts" do 
 
-    visit root_path
-    click_on "frank"
-    click_on "friends"
-    click_on "bob"
+    before(:each) do 
+      @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+      @post_1 = @bob.posts.create(body: "this is a post")
+      @post_2 = @bob.posts.create(body: "this is another post")
+      @frank = FactoryBot.create(:user, name: "frank", email: "frank@example.com")
+      @bob.friends << @frank
+      sign_in @frank
+
+      visit root_path
+      click_on "frank"
+      click_on "friends"
+      click_on "bob"
+    end
+
+    scenario "they see their friend's posts in descending order" do 
+      user_sees_content_order_descending(@post_1.body, @post_2.body)
+    end
+
+    scenario "they don't see a form to create a post" do 
+      user_doesnt_see_post_form
+    end
+
+    scenario "they see their friend's friends" do 
+      @sean = FactoryBot.create(:user, name: "sean", email: "sean@example.com")
+      @alex = FactoryBot.create(:user, name: "alex", email: "alex@example.com")
+      @bob.friends << [@sean, @alex]
+
+      click_on "friends"
+
+      expect(page).to have_content("frank")
+      expect(page).to have_content("sean")
+      expect(page).to have_content("alex")
+    end
+
+    scenario "they don't see an 'unfriend' link next to their friend's friends" do 
+      click_on "friends"
+
+      expect(page).not_to have_link("unfriend")
+    end
+
+    scenario "they don't see an edit link next to the friend's posts" do 
+      post_doesnt_appear_editable(@post_1)
+    end
+
+    scenario "they don't see a delete link next to the friend's posts" do 
+      post_doesnt_appear_deletable(@post_1)
+    end
+
+    scenario "they don't see the edit profile link" do 
+      expect(page).not_to have_link("edit profile")
+    end
+
+    scenario "they don't see the 'friend requests' link" do 
+      expect(page).not_to have_link("friend requests")
+    end
+
   end
 
-  scenario "they see their friend's posts in descending order" do 
-    user_sees_content_order_descending(@post_1.body, @post_2.body)
-  end
+  context "without posts" do 
 
-  scenario "they don't see a form to create a post" do 
-    user_doesnt_see_post_form
-  end
+    scenario "and sees 'no posts yet'" do 
+      @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+      @frank = FactoryBot.create(:user, name: "frank", email: "frank@example.com")
+      @bob.friends << @frank
+      sign_in @frank
 
-  scenario "they see their friend's friends" do 
-    @sean = FactoryBot.create(:user, name: "sean", email: "sean@example.com")
-    @alex = FactoryBot.create(:user, name: "alex", email: "alex@example.com")
-    @bob.friends << [@sean, @alex]
+      visit root_path
+      click_on "frank"
+      click_on "friends"
+      click_on "bob"
 
-    click_on "friends"
+      expect(page).to have_content("no posts yet")
+    end
 
-    expect(page).to have_content("frank")
-    expect(page).to have_content("sean")
-    expect(page).to have_content("alex")
-  end
-
-  scenario "they don't see an 'unfriend' link next to their friend's friends" do 
-    click_on "friends"
-
-    expect(page).not_to have_link("unfriend")
-  end
-
-  scenario "they don't see an edit link next to the friend's posts" do 
-    post_doesnt_appear_editable(@post_1)
-  end
-
-  scenario "they don't see a delete link next to the friend's posts" do 
-    post_doesnt_appear_deletable(@post_1)
-  end
-
-  scenario "they don't see the edit profile link" do 
-    expect(page).not_to have_link("edit profile")
-  end
-
-  scenario "they don't see the 'friend requests' link" do 
-    expect(page).not_to have_link("friend requests")
   end
 
 end
