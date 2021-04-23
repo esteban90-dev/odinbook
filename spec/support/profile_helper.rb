cb607a620023fba3564daff34d5e25cd32f765b8
Stephen Allard <stephencallard@gmail.com>
2021-04-22 20:38:19 -0500

    capitalize - button text is 'Create Post'

--- spec/support/profile_helper.rb
+++ spec/support/profile_helper.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 def user_doesnt_see_post_form
   expect(page).not_to have_field("Body")
   expect(page).not_to have_field("Picture")
-  expect(page).not_to have_button("create post")
+  expect(page).not_to have_button("Create Post")
 end
 
 def user_doesnt_see_comment_form
