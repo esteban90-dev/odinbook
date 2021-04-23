cf00a76711fdb55ae4d9a24447cb62398654e23e
Stephen Allard <stephencallard@gmail.com>
2021-04-22 20:56:22 -0500

    add feature - user sends friend request from non-friend's profile

--- spec/features/friend_request/requestor_sends_friend_request_spec.rb
+++ spec/features/friend_request/requestor_sends_friend_request_spec.rb
@@ -2,29 +2,63 @@ require 'rails_helper'
 
 feature "requestor sends friend request" do
 
-  before(:each) do 
-    @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
-    @joe = FactoryBot.create(:user, name: "joe", email: "joe@example.com")
+  context "from the users index" do 
 
-    sign_in @bob
-    visit root_path
-    click_on "users"
-    add_friend(@joe)
+    before(:each) do 
+      @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+      @joe = FactoryBot.create(:user, name: "joe", email: "joe@example.com")
+
+      sign_in @bob
+      visit root_path
+      click_on "users"
+      add_friend(@joe)
+    end
+
+    scenario "and sees a flash message" do
+      expect(page).to have_content("Friend request successfully sent")
+    end
+
+    scenario "and sees 'friend request sent' next to the requestee in the users index" do 
+      expect(page).to have_user_appear_pending(@joe)
+    end
+
+    scenario "and sees the request appear in the sent section of the friend requests index" do 
+      click_on "bob"
+      click_on "friend requests"
+  
+      expect(page).to have_sent_friend_request(@bob.sent_friend_requests.first)
+    end
+
   end
 
-  scenario "and sees a flash message" do
-    expect(page).to have_content("Friend request successfully sent")
-  end
+  context "from a non friend's profile" do 
 
-  scenario "and sees 'friend request sent' next to the requestee in the users index" do 
-    expect(page).to have_user_appear_pending(@joe)
-  end
+    before(:each) do 
+      @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+      @joe = FactoryBot.create(:user, name: "joe", email: "joe@example.com")
 
-  scenario "and sees the request appear in the sent section of the friend requests index" do 
-    click_on "bob"
-    click_on "friend requests"
- 
-    expect(page).to have_sent_friend_request(@bob.sent_friend_requests.first)
+      sign_in @bob
+      visit root_path
+      click_on "users"
+      click_on "joe"
+      find(add_friend_selector).click
+    end
+
+    scenario "and sees a flash message" do
+      expect(page).to have_content("Friend request successfully sent")
+    end
+
+    scenario "and sees 'friend request sent' next to the requestee in the users index" do 
+      expect(page).to have_user_appear_pending(@joe)
+    end
+
+    scenario "and sees the request appear in the sent section of the friend requests index" do 
+      click_on "bob"
+      click_on "friend requests"
+  
+      expect(page).to have_sent_friend_request(@bob.sent_friend_requests.first)
+    end
+
   end
 
 end
