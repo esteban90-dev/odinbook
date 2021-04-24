8bc67c20c97e6bab11d8d08fb49aa515212a0955
Stephen Allard <stephencallard@gmail.com>
2021-04-24 10:58:32 -0500

    requestee accepts friend request from the requestor's profile

--- spec/features/friend_request/requestee_accepts_friend_request_spec.rb
+++ spec/features/friend_request/requestee_accepts_friend_request_spec.rb
@@ -260,4 +260,91 @@ feature "requestee accepts friend request" do
 
   end
 
+  context "from the requestor's profile" do 
+
+    before(:each) do
+      @bob = FactoryBot.create(:user, name: "bob", email: "bob@example.com")
+      @joe = FactoryBot.create(:user, name: "joe", email: "joe@example.com")
+      
+      sign_in @bob
+      visit root_path
+      click_on "users"
+      add_friend(@joe)
+      sign_out @bob
+
+      sign_in @joe
+      visit root_path
+      click_on "users"
+      click_on "bob"
+      click_on "accept"
+    end
+
+    scenario "and sees a flash message" do
+      expect(page).to have_content("You and bob are now friends")
+    end
+
+    scenario "and sees the requestor appear in their friends list" do 
+      click_on "joe"
+      click_on "friends"
+
+      expect(page).to have_content("bob")
+    end
+
+    scenario "and sees the requestor appear in the user index as a friend" do 
+      click_on "users"
+
+      expect(page).to have_user_appear_as_friend(@bob)
+    end
+
+    scenario "and sees 'friends' on the requestor's profile" do 
+      click_on "joe"
+      click_on "friends"
+      click_on "bob"
+
+      friend_status_section = find('[data-test="friend-status"]')
+      expect(friend_status_section).to have_content("friends")
+    end
+
+    scenario "and no longer sees incoming friend requests" do 
+      click_on "joe"
+      click_on "friend requests"
+
+      expect(incoming_friend_requests.count).to eq(0)
+    end
+
+    scenario "the requestor receives a new notification" do 
+      sign_out @joe
+      sign_in @bob
+
+      visit root_path
+      click_on "notifications"
+
+      expect(page).to have_content("joe accepted your friend request")
+    end
+
+    scenario "the requestor sees the requestee in their friends list" do
+      sign_out @joe
+      sign_in @bob
+
+      visit root_path
+      click_on "bob"
+      click_on "friends"
+
+      expect(page).to have_content("joe")
+    end
+
+    scenario "the requestor no longer sees pending sent friend requests" do 
+      sign_out @joe
+      sign_in @bob
+
+      visit root_path
+      click_on "bob"
+      click_on "friend requests"
+
+      expect(sent_friend_requests.count).to eq(0)
+      expect(page).to have_content("no friend requests yet")
+    end
+  
+  end
+
 end
